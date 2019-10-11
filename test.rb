aab615ca03dfa24aa9bbe87f52050596af878cc5
Andrew Capp <acapp909@gmail.com>
2019-10-11 18:40:18 +0000

    save

--- test.rb
+++ test.rb
@@ -14,8 +14,14 @@ def array_test_2(array, value)
   else
     return false
   end
-
 end
+
+def array_test_3(array, num_of_random_elements)
+  #return random elements from array (based on specified #)
+  array.sample(num_of_random_elements)
+end
+
+
 
 
 
@@ -23,4 +29,5 @@ array = %w(dog cat bird hampster bear lion giraffe)
 array_2 = [7, 3, 5, "string", "None", 6, 4]
 
 #p array_test(array, "cats")
-p array_test_2(array_2, 4)
+#p array_test_2(array_2, 4)
+p array_test_3(array, 3)
