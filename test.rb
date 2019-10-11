0e72176895e29e98c23f99c65b8c74309315c4fe
Andrew Capp <acapp909@gmail.com>
2019-10-11 18:27:48 +0000

    -m "Save" :x!
    :X!

--- test.rb
+++ test.rb
@@ -8,7 +8,19 @@ def array_test(array, value)
 
 end
 
+def array_test_2(array, value)
+  if array.first == value || array.last == value
+    return true
+  else
+    return false
+  end
+
+end
+
+
 
 array = %w(dog cat bird hampster bear lion giraffe)
+array_2 = [7, 3, 5, "string", "None", 6, 4]
 
-p array_test(array, "cats")
+#p array_test(array, "cats")
+p array_test_2(array_2, 4)
